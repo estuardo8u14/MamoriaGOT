dbc9a507e0b5aae6835024860f0515935cf02e0a
estuardo8u14 <estuardo8u14@gmail.com>
2022-04-21 12:05:40 -0600

    formato airbnb

--- src/components/Memoria/Memoria.js
+++ src/components/Memoria/Memoria.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+
 import Carta from '../Carta/Carta';
 import './Memoria.css';
 import carta0 from '../../../public/assets/images/carta_0.png';
@@ -9,16 +9,13 @@ import carta3 from '../../../public/assets/images/carta3.png';
 import carta4 from '../../../public/assets/images/carta4.png';
 import carta5 from '../../../public/assets/images/carta5.png';
 
-
-
-
 const imagenesCartas = [
-	{'src': carta0, pareja: false},
-	{'src': carta1, pareja: false},
-	{'src': carta2, pareja: false},
-	{'src': carta3, pareja: false},
-	{'src': carta4, pareja: false},
-	{'src': carta5, pareja: false}
+	{ src: carta0, pareja: false },
+	{ src: carta1, pareja: false },
+	{ src: carta2, pareja: false },
+	{ src: carta3, pareja: false },
+	{ src: carta4, pareja: false },
+	{ src: carta5, pareja: false },
 ];
 
 function Memoria() {
@@ -28,19 +25,19 @@ function Memoria() {
 	const [eleccionDos, setEleccionDos] = useState(null);
 	const [noTocar, setNoTocar] = useState(false);
 
-	//Revolver cartas
+	// Revolver cartas
 	const revolverCartas = () => {
 		const revolvidasCartas = [...imagenesCartas, ...imagenesCartas]
 			.sort(() => Math.random() - 0.5)
 			.map((carta) => ({ ...carta, id: Math.random() }));
-    
+
 		setEleccionUno(null);
 		setEleccionDos(null);
 		setCartas(revolvidasCartas);
 		setTurnos(0);
 	};
 
-	//handle de eleccion
+	// handle de eleccion
 	const handleEleccion = (carta) => {
 		eleccionUno ? setEleccionDos(carta) : setEleccionUno(carta);
 	};
@@ -49,15 +46,12 @@ function Memoria() {
 		if (eleccionUno && eleccionDos) {
 			setNoTocar(true);
 			if (eleccionUno.src === eleccionDos.src) {
-				setCartas(prevCartas => {
-					return prevCartas.map(carta => {
-						if (carta.src === eleccionUno.src){
-							return {...carta, pareja: true};
-						}else {
-							return carta;
-						}
-					});
-				});
+				setCartas((prevCartas) => prevCartas.map((carta) => {
+					if (carta.src === eleccionUno.src) {
+						return { ...carta, pareja: true };
+					}
+					return carta;
+				}));
 				resetTurno();
 			} else {
 				setTimeout(() => resetTurno(), 1500);
@@ -68,7 +62,7 @@ function Memoria() {
 	const resetTurno = () => {
 		setEleccionUno(null);
 		setEleccionDos(null);
-		setTurnos(prevTruno => prevTruno + 1 );
+		setTurnos((prevTruno) => prevTruno + 1);
 		setNoTocar(false);
 	};
 
@@ -82,18 +76,20 @@ function Memoria() {
 			<h2>GAME OF THRONES</h2>
 			<button onClick={revolverCartas}>Empezar</button>
 			<div className="grid-cartas">
-				{cartas.map(carta =>(
-					<Carta 
-						key = {carta.id} 
+				{cartas.map((carta) => (
+					<Carta
+						key={carta.id}
 						carta={carta}
 						handleEleccion={handleEleccion}
-						vuelta={carta === eleccionUno || carta === eleccionDos || carta.pareja}      
+						vuelta={carta === eleccionUno || carta === eleccionDos || carta.pareja}
 						noTocar={noTocar}
 					/>
 				))}
 			</div>
 			<h3>
-        Turnos: {turnos}
+  Turnos: 
+				{' '}
+				{turnos}
 			</h3>
 			<h5>Imagenes dibujadas y juego creado por Estuardo Ureta 17010</h5>
 		</div>
